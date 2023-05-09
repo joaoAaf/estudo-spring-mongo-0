49ee516228bbe3aa42615de0c69dd6270c4906d4
joaoAaf <joao.anderson.af@gmail.com>
2023-05-08 21:48:18 -0300

    criação do projeto

---
+++ src/main/java/com/estudo/estudoSpringMongo/EstudoSpringMongoApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.estudoSpringMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoSpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoSpringMongoApplication.class, args);
+	}
+
+}
